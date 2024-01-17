7887e315c6bf0f6bb94aa5539c849398d03bde22
Precell <precell@uncommon.org>
2024-01-17 13:44:41 +0200

    union types

--- app.js
+++ app.js
@@ -1,54 +1,6 @@
-// const person: {
-//     name: string;
-//     age:number;
-// } = {
-//     name: 'Precell',
-//     age:26
-// }
-// const person: object = {
-//     name: 'Precell',
-//     age:26
-// }
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role:[number, string]
-// } = {
-//     name: 'Precell',
-//     age:26,
-//     hobbies:['sports', 'cooking'],
-//     role: [2, 'author']
-// }
-// person.role.push('admin', 'cooker')
-// person.role[1] = 10 ERROR!!!
-// person.role = [0, 'admin'. 'user'] ERROR !!!
-// Enums
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-// enum Role {ADMIN, READ_ONLY, AUTHOR}
-// enum Role {ADMIN = 5, READ_ONLY, AUTHOR}
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "ADMIN";
-    Role[Role["READ_ONLY"] = 200] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 300] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Precell',
-    age: 26,
-    hobbies: ['sports', 'cooking'],
-    role: Role.ADMIN
-};
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
+function combine(input1, input2) {
+    var result = input1 + input2;
+    return result;
 }
-var favoriteActivities;
-favoriteActivities = ['sports'];
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); ERROR!!!
-}
-console.log(person.name);
+var combineAges = combine(50, 60);
+console.log(combineAges);
--- app.ts
+++ app.ts
@@ -1,64 +1,10 @@
-// const person: {
-//     name: string;
-//     age:number;
-// } = {
-//     name: 'Precell',
-//     age:26
-// }
+function combine(input1: number, input2: number) {
+    const result = input1 + input2;
+    return result
+  }
 
-// const person: object = {
-//     name: 'Precell',
-//     age:26
-// }
+  const combineAges = combine(50, 60)
 
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role:[number, string]
-// } = {
-//     name: 'Precell',
-//     age:26,
-//     hobbies:['sports', 'cooking'],
-//     role: [2, 'author']
-// }
-
-// person.role.push('admin', 'cooker')
-// person.role[1] = 10 ERROR!!!
-// person.role = [0, 'admin'. 'user'] ERROR !!!
-
-
-// Enums
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-
-// enum Role {ADMIN, READ_ONLY, AUTHOR}
-// enum Role {ADMIN = 5, READ_ONLY, AUTHOR}
-enum Role {ADMIN = 'ADMIN', READ_ONLY = 200, AUTHOR = 300}
-
-
-const person = {
-    name: 'Precell',
-    age:26,
-    hobbies:['sports', 'cooking'],
-    role: Role.ADMIN
-}
-
-if (person.role === Role.ADMIN){
-    console.log('is admin');
-    
-}
-
-let favoriteActivities: string[]
-favoriteActivities = ['sports']
-
-for(const hobby of person.hobbies){
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); ERROR!!!
-    
-}
-
-console.log(person.name);
+  console.log(combineAges);
+  
+  
