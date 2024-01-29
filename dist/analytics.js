51e2dcc0ba60e0abc8ff78d1285e021385092de2
Precell <precell@uncommon.org>
2024-01-29 08:25:42 +0200

    sda

--- dist/analytics.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function sendAnalytics(data) {
-    console.log(data);
-}
-sendAnalytics("data");
-const button = document.querySelector('button');
-button.addEventListener("click", () => {
-    console.log('clicked');
-});
